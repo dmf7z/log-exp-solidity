bb0939f5f653808d7a9413963fece545f889596d
Daniel Fernandez <fernandez.daniel@gmail.com>
2020-08-05 17:45:20 -0300

    change int256 to uint256, update test

--- contracts/LogExpMath.sol
+++ contracts/LogExpMath.sol
@@ -2,32 +2,32 @@ pragma solidity ^0.5.7;
 
 library LogExpMath {
     
-    int256 constant PRECISION = 10**18; 
+    uint256 constant PRECISION = 10**18; 
    
-    int256 constant x0 = 128000000000000000000;
-    int256 constant a0 = 38877084059945950920000000000000000000000000000000000000;
-    int256 constant x1 = 64000000000000000000;
-    int256 constant a1 = 6235149080811616883000000000;
-    int256 constant x2 = 32000000000000000000;
-    int256 constant a2 = 78962960182680695160000000000000;
-    int256 constant x3 = 16000000000000000000;
-    int256 constant a3 = 8886110520507872637000000;
-    int256 constant x4 = 8000000000000000000;
-    int256 constant a4 = 2980957987041728275000;
-    int256 constant x5 = 4000000000000000000;
-    int256 constant a5 = 54598150033144239080;
-    int256 constant x6 = 2000000000000000000;
-    int256 constant a6 = 7389056098930650227;
-    int256 constant x7 = 1000000000000000000;
-    int256 constant a7 = 2718281828459045235;
-    int256 constant x8 = 500000000000000000;
-    int256 constant a8 = 1648721270700128146;
-    int256 constant x9 = 250000000000000000;
-    int256 constant a9 = 1284025416687741484;
+    uint256 constant x0 = 128000000000000000000;
+    uint256 constant a0 = 38877084059945950920000000000000000000000000000000000000;
+    uint256 constant x1 = 64000000000000000000;
+    uint256 constant a1 = 6235149080811616883000000000;
+    uint256 constant x2 = 32000000000000000000;
+    uint256 constant a2 = 78962960182680695160000000000000;
+    uint256 constant x3 = 16000000000000000000;
+    uint256 constant a3 = 8886110520507872637000000;
+    uint256 constant x4 = 8000000000000000000;
+    uint256 constant a4 = 2980957987041728275000;
+    uint256 constant x5 = 4000000000000000000;
+    uint256 constant a5 = 54598150033144239080;
+    uint256 constant x6 = 2000000000000000000;
+    uint256 constant a6 = 7389056098930650227;
+    uint256 constant x7 = 1000000000000000000;
+    uint256 constant a7 = 2718281828459045235;
+    uint256 constant x8 = 500000000000000000;
+    uint256 constant a8 = 1648721270700128146;
+    uint256 constant x9 = 250000000000000000;
+    uint256 constant a9 = 1284025416687741484;
 
-    function exp(int256 x) public pure returns (int256) {
-        int256 ans = PRECISION;
-        int256 last = 1;
+    function exp(uint256 x) public pure returns (uint256) {
+        uint256 ans = PRECISION;
+        uint256 last = 1;
         if(x >= x0) {
             last = a0;
             x -= x0;
@@ -68,8 +68,8 @@ library LogExpMath {
             ans = ans * a9 / PRECISION;
             x -=  x9;
         }
-        int256 s = PRECISION;
-        int256 t = x;
+        uint256 s = PRECISION;
+        uint256 t = x;
         s += t;
         t = (t * x / 2) / PRECISION;
         s += t;
